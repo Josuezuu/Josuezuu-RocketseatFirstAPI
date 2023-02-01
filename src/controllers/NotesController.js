ca62e366e4609b556973d07f9596f6b0ac094196
josue <josue-zu@hotmail.com>
2023-01-31 23:27:41 -0300

    autenticação

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex");
 class NotesController {
   async create(req, res) {
     const { title, description, tags, links } = req.body;
-    const { user_id } = req.params;
+    const user_id = req.user.id;
 
     const note_id = await knex("notes").insert({
       title,
@@ -51,7 +51,8 @@ class NotesController {
     return res.json();
   }
   async index(req, res) {
-    const { user_id, title, tags } = req.query;
+    const { title, tags } = req.query;
+    const user_id = req.user.id;
     let notes;
     if (tags) {
       const filterTags = tags.split(",").map((tag) => tag.trim());
