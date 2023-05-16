6383af2ed9036918bc77a3c39be52fad3593d286
josue <josue-zu@hotmail.com>
2023-05-15 22:04:59 -0300

    problema details

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -5,7 +5,7 @@ class NotesController {
     const { title, description, tags, links } = req.body;
     const user_id = req.user.id;
 
-    const note_id = await knex("notes").insert({
+    const [note_id] = await knex("notes").insert({
       title,
       description,
       user_id,
@@ -62,6 +62,7 @@ class NotesController {
         .whereLike("notes.title", `%${title}%`)
         .whereIn("name", filterTags)
         .innerJoin("notes", "notes.id", "tags.note_id")
+        .groupBy("notes.id")
         .orderBy("notes.title");
     } else {
       notes = await knex("notes")
@@ -78,7 +79,7 @@ class NotesController {
       };
     });
 
-    return res.json({ notesWithTags });
+    return res.json(notesWithTags);
   }
 }
 
